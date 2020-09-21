f7240c762f623d2b1f433dff254b3588afb07ba1
liwuyi <2020783236@qq.com>
2020-09-21 22:39:44 +0800

    项目搭建

---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <tab-bar>
+        <tab-bar-item path="/home" activeColor="red">
+            <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+            <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
+            <div slot="item-text">首页</div>
+        </tab-bar-item>
+        <tab-bar-item path="/category" activeColor="red">
+            <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+            <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
+            <div slot="item-text">分类</div>
+        </tab-bar-item>
+        <tab-bar-item path="/cart" activeColor="red">
+            <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
+            <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
+            <div slot="item-text">购物</div>
+        </tab-bar-item>
+        <tab-bar-item path="/profile" activeColor="red">
+            <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+            <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
+            <div slot="item-text">我的</div>
+        </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+    //路径比较深的话可以使用路径别名，看起来没有那么多../
+    import TabBar from "components/common/tabbar/TabBar";
+    import TabBarItem from "components/common/tabbar/TabBarItem";
+    // require("assets/iconfont/tabbar/iconfont");
+
+    export default {
+        name: "MainTabBar",
+        components: {
+            TabBar,
+            TabBarItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
+
+// 安装插件
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '',
+        name: '首页',
+        redirect: '/home'
+    },{
+        path: '/home',
+        name: '首页',
+        component: Home
+    },{
+        path: '/category',
+        name: '分类',
+        component: Category
+    },{
+        path: '/cart',
+        name: '购物车',
+        component: Cart
+    },{
+        path: '/profile',
+        name: '我的',
+        component: Profile
+    }
+];
+
+// 创建router
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+
+export default router
