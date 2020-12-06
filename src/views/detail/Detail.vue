594a57168ff090eae43f831b5e6bc945742af2ba
liwuyi <2020783236@qq.com>
2020-12-06 23:45:28 +0800

    详情页文件

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="detail">
+        <detail-nav-bar></detail-nav-bar>
+        <detail-swiper :top-images="topImages"/>
+    </div>
+</template>
+
+<script>
+    import DetailNavBar from './childComps/DetailNavBar'
+    import DetailSwiper from './childComps/DetailSwiper'
+    import {getGoodsDetail} from "../../network/detail";
+
+    export default {
+        name: "Detail",
+        data() {
+            return {
+                iid: null,
+                topImages: [],
+            }
+        },
+        components: {
+            DetailNavBar,
+            DetailSwiper
+        },
+        created() {
+            // console.log(this.$route.params.iid);
+            this.iid = this.$route.query.iid
+            getGoodsDetail(this.iid).then(res => {
+                this.topImages = res.result.itemInfo.topImages
+                console.log(this.topImages)
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
