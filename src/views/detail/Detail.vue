eed23ce309475fb28ee8df26e196a1dda3f58df2
liwuyi <2020783236@qq.com>
2020-12-07 23:00:07 +0800

    引入商品信息组件

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,13 +2,15 @@
     <div id="detail">
         <detail-nav-bar></detail-nav-bar>
         <detail-swiper :top-images="topImages"/>
+        <detail-base-info :goods="goods"/>
     </div>
 </template>
 
 <script>
     import DetailNavBar from './childComps/DetailNavBar'
     import DetailSwiper from './childComps/DetailSwiper'
-    import {getGoodsDetail} from "../../network/detail";
+    import DetailBaseInfo from './childComps/DetailBaseInfo'
+    import {getGoodsDetail, Goods} from "../../network/detail";
 
     export default {
         name: "Detail",
@@ -16,18 +18,22 @@
             return {
                 iid: null,
                 topImages: [],
+                goods: {}
             }
         },
         components: {
             DetailNavBar,
-            DetailSwiper
+            DetailSwiper,
+            DetailBaseInfo,
         },
         created() {
             // console.log(this.$route.params.iid);
             this.iid = this.$route.query.iid
             getGoodsDetail(this.iid).then(res => {
-                this.topImages = res.result.itemInfo.topImages
+                const data = res.result
+                this.topImages = data.itemInfo.topImages
                 console.log(this.topImages)
+                this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
             })
         }
     }
